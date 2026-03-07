c40078835b2dedf05a89b06d0bb176935eef6c32
godstime01 <specialgodstym@gmail.com>
2026-03-07 08:11:49 +0100

    updated the landing page

--- apps/public_app/urls.py
+++ apps/public_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import LandingPageView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path("", LandingPageView.as_view())
+    path("", LandingPageView.as_view()),
+    path("pricing", TemplateView.as_view(template_name="pricing.html"), name="pricing"),
 ]
