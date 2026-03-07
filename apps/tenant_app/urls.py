3ac5bd859aaee67ae7f7732edb5041fce328ee6f
godstime01 <specialgodstym@gmail.com>
2026-03-07 11:27:28 +0100

    added template for dashboard overview

--- apps/tenant_app/urls.py
+++ apps/tenant_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('accounts/dashboard/', views.GymDashboardOveriew.as_view(), name='home'),
+    path('accounts/dashboard/', views.GymDashboardOveriew.as_view(), name='dashboard_overview'),
     path('dashboard/members/', views.MemberList.as_view(), name='members_list'),
     path('dashboard/members/add/', views.NewMemberView.as_view(), name='members_add'),
     path("dashboard/reports/", views.ReportsDataView.as_view(), name="reports_data"),
