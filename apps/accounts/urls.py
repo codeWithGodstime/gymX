d31cdf74c0c53bded6fde410ad9e79b0c9fce505
godstime01 <specialgodstym@gmail.com>
2026-03-07 07:32:37 +0100

    updated the landing page

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -11,7 +11,7 @@ from .views import (
 urlpatterns = [
     path("accounts/logout/", TenantLogoutView.as_view(), name='accounts_logout'),
     path("accounts/login/", TenantLoginView.as_view(), name='accounts_login'),
-    path("accounts/signup/", GymOwnerSignupView.as_view()),
+    path("accounts/signup/", GymOwnerSignupView.as_view(), name='accounts_signup'),
     path("accounts/subscription/", SubscriptionView.as_view(), name='subscription'),
     path("accounts/payment/initialize/", InitializePaymentView.as_view(), name='initialize_payment'),
     path("accounts/payment/callback/", PaymentCallbackView.as_view(), name='payment_callback'),
