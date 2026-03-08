50adf2e0651d11e2ce012156c8308c10c90768e8
godstime01 <specialgodstym@gmail.com>
2026-03-08 17:44:40 +0100

    kse

--- apps/public_app/management/commands/setup_public_tenant.py
+++ apps/public_app/management/commands/setup_public_tenant.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django_tenants.utils import schema_context
+from django.conf import settings
 from apps.public_app.models import Gym, Domain
 
 
@@ -7,9 +8,9 @@ class Command(BaseCommand):
     help = 'Create public gym and localhost domain if they do not exist'
 
     def handle(self, *args, **options):
-        # Switch to public schema to create the tenant
+        host = settings.DOMAIN_HOST
+        host = host.replace("http://", "").replace("https://", "").split(":")[0]
         with schema_context('public'):
-            # Check if public gym exists
             public_gym = Gym.objects.filter(schema_name='public').first()
 
             if not public_gym:
@@ -25,21 +26,20 @@ class Command(BaseCommand):
             else:
                 self.stdout.write(f'Public gym already exists: {public_gym.name}')
 
-            # Check if localhost domain exists for this gym
-            localhost_domain = Domain.objects.filter(
-                domain='gymx.local',
+            domain = Domain.objects.filter(
+                domain=host,
                 tenant=public_gym
             ).first()
 
-            if not localhost_domain:
-                self.stdout.write('Creating localhost domain...')
-                localhost_domain = Domain.objects.create(
-                    domain='gymx.local',
+            if not domain:
+                self.stdout.write(f'Creating domain {host}...')
+                Domain.objects.create(
+                    domain=host,
                     tenant=public_gym,
                     is_primary=True
                 )
                 self.stdout.write(
-                    self.style.SUCCESS(f'Successfully created localhost domain for {public_gym.name}')
+                    self.style.SUCCESS(f'Successfully created domain {host} for {public_gym.name}')
                 )
             else:
-                self.stdout.write(f'Localhost domain already exists for {public_gym.name}')
+                self.stdout.write(f'Domain {host} already exists for {public_gym.name}')
